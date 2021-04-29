29abf426ea17bbb78278707898312a0b61a9d0a8
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-04-29 18:07:59 +0200

    Add exercice 7

---
+++ Módulo2/modulo2-leccion1.1-ejercicio3/main5.js
@@ -0,0 +1,4 @@
+const passElement = document.querySelector('.pass');
+passElement.innerHTML = passElement.innerHTML + 'Ada2020';
+
+passElement.innerHTML = passElement.innerHTML + '****';
---
+++ Módulo2/modulo2-leccion1.1-ejercicio3/main7.js
@@ -0,0 +1,5 @@
+const listElement = document.querySelector('.list');
+
+const content = '<li><a href="#">text1</a></li><li><a href="#">texto2</a></li><li><a href="#">texto3</a></li>';
+
+listElement.innerHTML = content;
