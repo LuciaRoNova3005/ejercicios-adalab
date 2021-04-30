9e3e0f64848037c50fcf7a07198fdb2b8a35fce8
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-04-30 09:48:14 +0200

    Add Exercice 8

--- Módulo2/modulo2-leccion1.1-ejercicio3/main8.js
+++ Módulo2/modulo2-leccion1.1-ejercicio3/main8.js
@@ -1,5 +1,8 @@
-'user strict';
+'use strict';
 const sectionA = document.querySelector('.section-a');
 const sectionB = document.querySelector('.section-b');
 
-sectionA.classList.add('.background');
+sectionA.classList.add('background');
+
+console.log(sectionA);
+console.log(sectionB);
