2226ffc9f33947dac3c6d92d24712188425f400e
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-02 17:07:12 +0200

    Test exercice

--- Módulo2/módulo2-leccion2.2./main9.js
+++ Módulo2/módulo2-leccion2.2./main9.js
@@ -2,4 +2,4 @@
 const nombre = document.querySelector ('.js-name');
 const contenName = nombre.innerHTML;
 
-console.log(nombre.innerHTML + 'esta compuesto por ' +  contenName.length + ' caracteres');
+console.log(nombre.innerHTML + ' esta compuesto por ' +  contenName.length + ' caracteres');
