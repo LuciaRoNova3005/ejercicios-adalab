77987ee2f19d73e3879aea40dc950b5dc69e97dd
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-05 18:14:39 +0200

    Exercice leccion 1.3

---
+++ Módulo2/modulo2-leccion1.3/main4.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const age = 2;
+
+if (age = 1){
+    console.log ('Tu perro tiene 15 años')
+} else if (age = 2) {
+    console.log ('Tu perro tiene + ${age} + 15')
+}
+else if (age >= 3) {
+    console.log ('Tu perro tiene ${age}*5')
+}
---
+++ Módulo2/modulo2-leccion2.4/main.js
@@ -0,0 +1,52 @@
+'use strict';
+function mult(a,b){
+    return a * b;
+}
+const result = mult(4,3);
+console.log (result);
+
+ //Ejercio 2//
+function media (a,b,c,d){
+     return ((a+b+c+d)/2);
+ }
+ const mediaResul = media (4,4,4,4);
+ console.log (mediaResul);
+ //Ejercio 3//
+ function Iva(a){
+     return 'Precio sin Iva= '+ a + ' Iva 2.1 ' + 'Total ' + ((a*21/100) + a)
+ }
+const price= Iva (10);
+console.log (price)
+
+ //Ejercio 4//
+ function par(a){
+    if ( a % 2 == 0 )
+        {
+            return "True";
+        }
+        else
+        {
+            return "False";
+        }
+}
+
+const number = par (5);
+console.log (number)
+
+ //Ejercio 5// 
+ function getEl (a){
+ return document.querySelector(a).innerHTML;
+ 
+}
+
+ const clase = getEl('.btn');
+ console.log (clase)
+
+ //Ejercicio 7//
+ const ejercio7 = parseInt(c)
+
+
+
+ //Ejercicio 8//
+ 
+ 
