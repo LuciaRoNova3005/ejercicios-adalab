8fd5d95f2b8417b4739833ad7fa7a1e1da2f0698
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-06 20:08:27 +0200

    Add exercice 3

--- Módulo2/módulo2-leccion2.5/main.js
+++ Módulo2/módulo2-leccion2.5/main.js
@@ -8,5 +8,21 @@ function showAlert(button){
 button.addEventListener('click', showAlert);
 
 
+//Ejercicio 2//
+const button2 = document.querySelector (".js-input");
+function Form (button2){
+  console.log (button2.value = "Mi primer click, ¡ole yo y la mujer que me parió!")
+}
 
+button2.addEventListener('click', Form);
 
+//Ejercicio 3"//
+const paragraph = document.querySelector (".show")
+const paragraph2 = document.querySelector (".par2")
+ function mostrar () {
+    if (paragraph2.classList.contains('hidden')) {
+        (paragraph2.classList.remove('hidden'));
+      }
+      console.log (mostrar(paragraph2))
+ }
+ paragraph.addEventListener('mouseover', mostrar);
