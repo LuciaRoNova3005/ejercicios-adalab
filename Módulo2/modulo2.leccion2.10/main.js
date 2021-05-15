d9043b8966c574240198f22b4e214d8d4acec86c
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-15 10:28:51 +0200

    Add exercice 2

--- Módulo2/modulo2.leccion2.10/main.js
+++ Módulo2/modulo2.leccion2.10/main.js
@@ -1,11 +1,27 @@
 "use strict";
 
 //Ejercicio 1//
-function EnterNumber() {
+const button = document.querySelector(".js-number");
+function enterNumber() {
   fetch("https://api.rand.fun/number/integer")
     .then((response) => response.json())
     .then((data) => {
       document.querySelector(".js-result").innerHTML = data.result;
     });
 }
-document.querySelector(".js-number").addEventListener("click", EnterNumber);
+button.addEventListener("click", enterNumber);
+
+//Ejercicio 2//
+
+const buttonImg = document.querySelector(".js-imgbutton");
+
+function getChiguagua() {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      const img = document.querySelector("img");
+      img.src = data.message;
+      img.alt = data.success;
+    });
+}
+buttonImg.addEventListener("click", getChiguagua);
