1737b348ed5e8dd90f930fe3900cb77a98ac4d89
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-17 11:25:25 +0200

    Test exerice 6

--- Módulo2/modulo2.leccion2.10/main5.js
+++ Módulo2/modulo2.leccion2.10/main5.js
@@ -1,25 +1,45 @@
 "use script";
 const button = document.querySelector(".js-button");
+const text = document.querySelector(".js-parraf");
+const randonNumber = getRandomNumber(95);
+console.log(randonNumber);
 
-  //Ejercicio 6/
-  function getBreedsImage() {
-    fetch("https://dog.ceo/api/breeds/list")
-      .then((breedsResponse) => breedsResponse.json())
-      .then((breedsData) => {
-        const breeds = breedsData.message;
-        return fetch(`https://api.rand.fun/number/integer`);
-      })
-      .then((randomNumberResponse) => randomNumberResponse.json())
-      .then(((randomNumberData) => {
-        const number = randomNumberData.result
-        return fetch( `https://dog.ceo/api/breed/${breeds[number]}/images/random`);
-      })
-      .then((imageResponse) => imageResponse.json())
-      .then((imageData) => {
-        const img = document.querySelector("img");
-        img.src = imageData.message;
-        img.alt = "Perro";
-      })
-  }
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+//Ejercicio 6/
+function getBreedsImage() {
+  fetch("https://dog.ceo/api/breeds/list")
+    .then((breedsResponse) => breedsResponse.json())
+    .then((breedsData) => {
+      const breeds = breedsData.message;
+
+      console.log(breedsData.message[randonNumber]);
+      for (const randobreed in breeds) {
+        n;
+        text.innerHTML += ``;
+      }
+    });
+
+  // fetch("https://api.rand.fun/number/integer")
+  //   .then((responseNumber) => responseNumber.json())
+  //   .then((data) => {
+  //     i = data.result;
+  //     console.log(data.result);
+  //   });
+}
+//     .then((randomNumberResponse) => randomNumberResponse.json())
+//     .then(((randomNumberData) => {
+//       const number = randomNumberData.result
+//       return fetch( `https://dog.ceo/api/breed/${breeds[number]}/images/random`);
+//     })
+//     .then((imageResponse) => imageResponse.json())
+//     .then((imageData) => {
+//       const img = document.querySelector("img");
+//       img.src = imageData.message;
+//       img.alt = "Perro";
+//     })
+// }
 
 button.addEventListener("click", getBreedsImage);
