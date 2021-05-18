b12c93a172738f9ed52b67059978c6995a1358f9
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-18 17:16:19 +0200

    Add exercice

--- Módulo2/modulo2-leccion2.12/main.js
+++ Módulo2/modulo2-leccion2.12/main.js
@@ -8,3 +8,26 @@ for (let i = 0; i < numbers.length; i++) {
   container.appendChild(list);
   console.log(container);
 }
+
+//Ejercicio 2//
+const city = document.querySelector(".js-city");
+const containerImg = document.querySelector(".js-container");
+
+function addImage(url) {
+  const images = document.createElement("img");
+  containerImg.appendChild(images);
+  images.src = url;
+}
+
+function selectCity(event) {
+  if (event.target.value === "Madrid") {
+    addImage(
+      "https://www.institutfrancais.es/madrid/wp-content/uploads/sites/6/2019/09/banner-madrid-1920x1280.jpg"
+    );
+  } else if (event.currentTarget.value === "Paris") {
+    addImage(
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtXCu4U6rPKJbW__1b5XADM2haJ-0hE1eEnDrh0-0qgC9fw5Ow68KWvwTbc4S-N7nWijU&usqp=CAU"
+    );
+  }
+}
+city.addEventListener("change", selectCity);
