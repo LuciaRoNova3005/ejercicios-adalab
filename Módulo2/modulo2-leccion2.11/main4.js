53695d2609eebd873213198508588035026057bb
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-19 17:05:40 +0200

    Add exercice 1 and 2

--- Módulo2/modulo2-leccion2.11/main4.js
+++ Módulo2/modulo2-leccion2.11/main4.js
@@ -1,11 +1,16 @@
 "use strict";
 const nameUser = document.querySelector(".js-name");
 const text = document.querySelector(".js-result");
+const loadPage = document.querySelector("body");
 
 function content() {
   const valor = nameUser.value;
   text.innerHTML = valor;
-  localStorage.setItem("name", nameUser.value);
+  let local = localStorage.setItem("name", nameUser.value);
 }
-
+function getLocal() {
+  let localstore = localStorage.getItem;
+  console.log(localstore);
+}
+getLocal();
 nameUser.addEventListener("keyup", content);
---
+++ Módulo2/modulo2-leccion2.13/main.js
@@ -0,0 +1,8 @@
+"use strict";
+const marks = [5, 4, 6, 7, 9];
+const inflatedMarks = marks.map((item) => item + 1);
+console.log(inflatedMarks);
+//Ejercio 2//
+const names = ["María", "Lucía", "Susana", "Rocío", "Inmaculada"];
+const hello = names.map((item) => `Hola ${item}`);
+console.log(hello);
