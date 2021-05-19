32df93ee9a108685fb016f78278ca3b75b1c59ba
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-19 21:46:32 +0200

    Add exercice 4 leccion 2.11

--- Módulo2/modulo2-leccion2.11/main4.js
+++ Módulo2/modulo2-leccion2.11/main4.js
@@ -1,16 +1,15 @@
 "use strict";
 const nameUser = document.querySelector(".js-name");
 const text = document.querySelector(".js-result");
-const loadPage = document.querySelector("body");
 
 function content() {
   const valor = nameUser.value;
   text.innerHTML = valor;
-  let local = localStorage.setItem("name", nameUser.value);
+  localStorage.setItem("name", nameUser.value);
 }
-function getLocal() {
-  let localstore = localStorage.getItem;
-  console.log(localstore);
+let local = localStorage.getItem("name", nameUser.value);
+if (local !== "") {
+  nameUser.value = local;
+  text.innerHTML = local;
 }
-getLocal();
 nameUser.addEventListener("keyup", content);
