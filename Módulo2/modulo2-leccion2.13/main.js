8016ad0527885e3a21494c9806b26db142e67d37
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-19 19:05:20 +0200

    Add excerice 8

--- Módulo2/modulo2-leccion2.13/main.js
+++ Módulo2/modulo2-leccion2.13/main.js
@@ -34,3 +34,27 @@ const pins = [2389, 2384, 2837, 5232, 8998];
 
 const enter = pins.filter((pins) => pins % 2 === 0);
 console.log(enter);
+
+const passuser = [
+  { name: "María", isPremium: false, pin: 2389 },
+  { name: "Lucía", isPremium: true, pin: 2384 },
+  { name: "Susana", isPremium: true, pin: 2837 },
+  { name: "Rocío", isPremium: false, pin: 5232 },
+  { name: "Inmaculada", isPremium: false, pin: 8998 },
+];
+const numberPin = passuser.filter((passuser) => passuser.pin % 2 === 0);
+console.log(numberPin);
+
+//Ejercicio 7//
+
+const times = [56, 9, 45, 28, 35];
+const media = times.reduce((acc, total) => acc + total) / times.length;
+console.log(media);
+//Ejercicio 8//
+const runners = [
+  { name: "Gregory Goyle", time: 56, student: true },
+  { name: "Nymphadora Tonks", time: 9, student: false },
+  { name: "Luna Lovegood", time: 45, student: true },
+  { name: "Cedric Diggory", time: 28, student: true },
+  { name: "Cho Chang", time: 35, student: true },
+];
