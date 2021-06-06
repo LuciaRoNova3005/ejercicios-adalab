8838a84a095095a39c5de0af89c847af389448f1
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-06-06 12:13:26 +0200

    Add exercice1

--- Módulo 3/modulo3-leccion6/src/components/Exercice1.js
+++ Módulo 3/modulo3-leccion6/src/components/Exercice1.js
@@ -3,14 +3,22 @@ import "../stylesheets/App.css";
 
 const Exercice1 = () => {
   const [age, setNumber] = useState(20);
+  const [presents, setPresents] = useState(1);
+
   const handelClick = () => {
     setNumber(age + 1);
+  };
+
+  const handelPresent = () => {
+    setPresents(presents + 1);
   };
 
   return (
     <div>
       <p>Hoy tengo {age} años de edad</p>
       <button onClick={handelClick}>HAZME MÁS VIEJO</button>
+      <p>Tengo {presents} regalo</p>
+      <button onClick={handelPresent}>DAME MÁS REGALOS</button>
     </div>
   );
 };
