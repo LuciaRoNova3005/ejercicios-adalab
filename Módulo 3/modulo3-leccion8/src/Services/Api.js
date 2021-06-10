32708cf3274635f3f1e23e342afe4800b589157f
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-06-10 18:11:41 +0200

    Exercice1

--- Módulo 3/modulo3-leccion8/src/Services/Api.js
+++ Módulo 3/modulo3-leccion8/src/Services/Api.js
@@ -1,9 +1,18 @@
-const Api = () => {
-  return fetch("https://randomuser.me/api/?results=50")
+import "../stylesheets/App.css";
+/*const dataResults = [];*/
+const fetchLink = "https://randomuser.me/api/?results=50";
+
+const FetchDirectory = () => {
+  return fetch(fetchLink)
     .then((response) => response.json())
     .then((data) => {
+      /*const alldatas = data.results;
+
+      for (const alldata of alldatas) {
+        dataResults.push(alldata);
+      }
+*/
       return data;
     });
 };
-
 export default Api;
--- Módulo 3/modulo3-leccion8/src/components/App.js
+++ Módulo 3/modulo3-leccion8/src/components/App.js
@@ -1,8 +1,33 @@
 import "../stylesheets/App.css";
-import Api from "../Services/Api";
+import React from "react";
+import Api from "../Services/Api.js";
 
-function App() {
-  return <Api />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { people: [] };
+
+    FetchDirectory().then((data) => {
+      this.setState({
+        people: data.results,
+      });
+    });
+    this.renderPeople = this.renderPeople.bind(this);
+  }
+
+  renderPeople() {
+    return this.state.people.map((person, index) => {
+      return (
+        <li key={index}>
+          {person.name.title}, {person.name.first}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return <ul>{this.renderPeople()}</ul>;
+  }
 }
 
 export default App;
